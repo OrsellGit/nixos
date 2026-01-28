661df11238e2b9d4dd4c1c055895e23d6c7431f9
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2026-01-28 13:46:57 -0800

    hm/git: Updated to use the newer settings format

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -4,18 +4,20 @@
   programs = {
     git = {
       enable = true;
-      userName  = "OrsellGaming";
-      userEmail = "34631691+OrsellGaming@users.noreply.github.com";
-      lfs.enable = true;
-      extraConfig = {
-        commit.gpgsign = true;
-        gpg.format = "ssh"; #? Sign all commits using a SSH key from 1Password
-        "gpg \"ssh\"".program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
-        user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHISguK84EGMZZpL/Iz6Ug9AEP4BpSw3xIbamAmHgr/l";
-        pull.rebase = false;
-        push.autoSetupRemote = true;
-        core.autocrlf = "input";
+      settings = {
+        user.name  = "OrsellGaming";
+        user.email = "34631691+OrsellGaming@users.noreply.github.com";
+        extraConfig = {
+          commit.gpgsign = true;
+          gpg.format = "ssh"; #? Sign all commits using a SSH key from 1Password
+          "gpg \"ssh\"".program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+          user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHISguK84EGMZZpL/Iz6Ug9AEP4BpSw3xIbamAmHgr/l";
+          pull.rebase = false;
+          push.autoSetupRemote = true;
+          core.autocrlf = "input";
+        };
       };
+      lfs.enable = true;
     };
 
     lazygit.enable = true;
