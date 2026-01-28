48c54906904a47fec1c96fd2d48a4131313b56e6
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2026-01-28 13:55:37 -0800

    flake: Switched back to stable NixOS and Home Manager

--- flake.nix
+++ flake.nix
@@ -2,13 +2,17 @@
   description = "NixOS configuration with Home Manager";
 
   inputs = {
+    # envycontrol = {
+    #   url = github:bayasdev/envycontrol;
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     millennium = {
@@ -16,18 +20,13 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixcord = {
-      url = "github:kaylorben/nixcord";
+      url = "github:FlameFlag/nixcord";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-xr.url = "github:nix-community/nixpkgs-xr";
-    # sops-nix = {
-    #   url = "github:Mic92/sops-nix";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
-  outputs = { self, nixpkgs, flake-utils, home-manager, /*sops-nix,*/ nixpkgs-xr, ... }@inputs: {
+  outputs = { self, nixpkgs, flake-utils, home-manager, ... }@inputs: {
     nixosConfigurations = {
       lambda-core = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -35,16 +34,13 @@
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
-          #sops-nix.nixosModules.sops
           {
-            nixpkgs.overlays = [ nixpkgs-xr.overlays.default ];
             home-manager.backupFileExtension = "backup";
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.orsell = import ./home.nix;
             home-manager.sharedModules = [
               inputs.nixcord.homeModules.nixcord
-              #inputs.sops-nix.homeManagerModule
             ];
             home-manager.extraSpecialArgs = { inherit inputs; };
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
